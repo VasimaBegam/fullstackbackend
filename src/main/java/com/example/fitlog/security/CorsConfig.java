f9c6ed4b968ff4ac9c3b40d41e3bb1e10d311381
VasimaBegam <vasimaias2006@gmail.com>
2026-07-08 16:39:31 +0530

    Update CorsConfig.java

--- src/main/java/com/example/fitlog/security/CorsConfig.java
+++ src/main/java/com/example/fitlog/security/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
                 "http://localhost:5000",
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
-             "https://vasimabegam.github.io/fullstackfrontend/"
+             "https://vasimabegam.github.io"
         ));
 
         config.setAllowedMethods(List.of(
